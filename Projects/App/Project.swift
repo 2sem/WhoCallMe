34f0b140f31e8b92896e8990e73198a38fd1fe50
leesam <toyboy2@hanmail.net>
2024-06-09 18:21:52 +0900

    fix: adds additional info properties

--- Projects/App/Project.swift
+++ Projects/App/Project.swift
@@ -1,6 +1,42 @@
 import ProjectDescription
 import ProjectDescriptionHelpers
 
+let skAdNetworks: [Plist.Value] = ["cstr6suwn9",
+                                   "4fzdc2evr5",
+                                   "2fnua5tdw4",
+                                   "ydx93a7ass",
+                                   "5a6flpkh64",
+                                   "p78axxw29g",
+                                   "v72qych5uu",
+                                   "c6k4g5qg8m",
+                                   "s39g8k73mm",
+                                   "3qy4746246",
+                                   "3sh42y64q3",
+                                   "f38h382jlk",
+                                   "hs6bdukanm",
+                                   "prcb7njmu6",
+                                   "wzmmz9fp6w",
+                                   "yclnxrl5pm",
+                                   "4468km3ulz",
+                                   "t38b2kh725",
+                                   "7ug5zh24hu",
+                                   "9rd848q2bz",
+                                   "n6fk4nfna4",
+                                   "kbd757ywx3",
+                                   "9t245vhmpl",
+                                   "2u9pt9hc89",
+                                   "8s468mfl3y",
+                                   "av6w8kgt66",
+                                   "klf5c3l5u5",
+                                   "ppxm28t8ap",
+                                   "424m5254lk",
+                                   "uw77j35x4d",
+                                   "e5fvkxwrpn",
+                                   "zq492l623r",
+                                   "3qcr597p9d"
+    ]
+    .map{ .dictionary(["SKAdNetworkIdentifier" : "\($0).skadnetwork"]) }
+
 let project = Project(
     name: "App",
     packages: [
@@ -24,12 +60,21 @@ let project = Project(
             product: .app,
             bundleId: "com.credif.who",
             deploymentTargets: .iOS("13.0"),
-            infoPlist: .extendingDefault(
-                with: [
-                    "UILaunchStoryboardName": "LaunchScreen.storyboard",
-                    "UIMainStoryboardFile": "Main",
-                ]
-            ),
+        infoPlist: .extendingDefault(
+            with: [
+                "UILaunchStoryboardName": "LaunchScreen.storyboard",
+                "UIMainStoryboardFile": "Main",
+                "GADApplicationIdentifier": "ca-app-pub-9684378399371172~4206633246",
+                "GADUnitIdentifiers" : ["FullAd" : "ca-app-pub-9684378399371172/4108901647"],
+                "GoogleADUnitID": ["FullAd" : "ca-app-pub-9684378399371172/4108901647",
+                                   "RewardAd" : "ca-app-pub-9684378399371172/5771706946",
+                                   "TopBanner" : ""],
+                "Itunes App Id": "395429781",
+                "NSContactsUsageDescription": "This app needs access contacts to convert",
+                "NSUserTrackingUsageDescription": "Your data will be used to deliver personalized ads to you",
+                "SKAdNetworkItems": .array(skAdNetworks)
+            ]
+        ),
             sources: ["Sources/**"],
             resources: ["Resources/**"],
             entitlements: .file(path: .relativeToCurrentFile("Sources/App.entitlements")),
