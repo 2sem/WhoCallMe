3d55513713a6c20f6422b40af810d466bab9ef65
leesam <toyboy2@hanmail.net>
2024-06-30 22:18:34 +0900

    fix: remove Pods

--- Pods/Target Support Files/GADManager/GADManager-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_GADManager : NSObject
-@end
-@implementation PodsDummy_GADManager
-@end
--- Pods/Target Support Files/Pods-ThirdParty-ThirdPartyTests/Pods-ThirdParty-ThirdPartyTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ThirdParty_ThirdPartyTests : NSObject
-@end
-@implementation PodsDummy_Pods_ThirdParty_ThirdPartyTests
-@end
