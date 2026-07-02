6c50be60d11c1d728baef270d77a45966a1938e3
leesam <toyboy2@hanmail.net>
2026-07-02 15:44:09 +0900

    chore: upgrade Firebase to 12.15.0

--- Projects/App/Project.swift
+++ Projects/App/Project.swift
@@ -42,6 +42,8 @@ let project = Project(
     packages: [
         .remote(url: "https://github.com/2sem/GADManager",
                 requirement: .upToNextMajor(from: "1.3.8")),
+        .package(id: "firebase.firebase-ios-sdk", from: "12.15.0"),
+        .package(id: "google.googleappmeasurement", from: "12.15.0"),
         // .remote(url: "https://github.com/firebase/firebase-ios-sdk",
         //        requirement: .upToNextMajor(from: "10.4.0")),
         
@@ -109,11 +111,25 @@ let project = Project(
                 )
             ],
             dependencies: [
-                           .Projects.ThirdParty,
-                           .Projects.DynamicThirdParty,
-                           .package(product: "GADManager", type: .runtime)
+                            .Projects.ThirdParty,
+                            .Projects.DynamicThirdParty,
+                            .package(product: "GADManager", type: .runtime),
+                            .package(product: "FirebaseCore"),
+                            .package(product: "FirebaseCrashlytics"),
+                            .package(product: "FirebaseAnalytics"),
+                            .package(product: "GoogleAppMeasurement"),
+                            .package(product: "GoogleAppMeasurementCore"),
+                            .package(product: "GoogleAppMeasurementIdentitySupport"),
+                            .package(product: "FirebaseInstallations"),
+                            .package(product: "GULAppDelegateSwizzler"),
+                            .package(product: "GULMethodSwizzler"),
+                            .package(product: "GULNSData"),
+                            .package(product: "GULNetwork"),
+                            .package(product: "nanopb")
             ],
-            settings: .settings(configurations: [
+            settings: .settings(base: [
+                "OTHER_LDFLAGS": "$(inherited) -framework GoogleAppMeasurement -framework GoogleAppMeasurementIdentitySupport"
+            ], configurations: [
                 .debug(name: "Debug", xcconfig: "Configs/app.debug.xcconfig"),
                 .release(name: "Release", xcconfig: "Configs/app.release.xcconfig")
             ])
